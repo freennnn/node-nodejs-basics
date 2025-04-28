3836ffb577b81ae47fd970513ff90ff60b3de392
freen <freennnn@gmail.com>
2025-04-28 23:20:23 +0200

    fix: updated for 2025

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,7 @@
-import { fork, spawn } from 'child_process'
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { pipeline } from 'stream';
+import { fork, spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream";
 
 const dirName = path.dirname(fileURLToPath(import.meta.url));
 const childPath = path.join(dirName, "files", "script.js");
@@ -9,17 +9,9 @@ const childPath = path.join(dirName, "files", "script.js");
 const spawnChildProcess = async (args) => {
   let child = fork(childPath, args);
 
-  process.stdin.on('message', (data) => child.send(data));
-  child.on('message', (data) => console.log(`Received from child process to main stdout: ${data}`));
-
-  // If we used spawn or {options.stdio : 'pipe'/'inherit'} we would have stoud/stdin in child process
-  // process.stdin.pipe(child.stdin);
-  // child.stdout.pipe(process.stdout);
-  // child.stdout.on('data', (data) => {
-  //   console.log(`from child to master stdout:\n ${data.toString()}`);
-  // });
-
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(['one', 'two']);
+spawnChildProcess(["one", "two"]);
